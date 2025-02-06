daabdc4797586745ba1226cba16a506b77726c6e
Akshat Pandey <akshatp1903@gmail.com>
2025-02-07 00:00:34 +0530

    new assignment

---
+++ Week2/BalanceSort/model/SortedBalance.java
@@ -0,0 +1,23 @@
+//package com.aurionpro.model;
+package BalanceSort.model;
+
+public class SortedBalance {
+	private String accountNumber;
+    private String name;
+    private double balance;
+
+    public SortedBalance(String accountNumber, String name, double balance) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public double getBalance(){ 
+    	return balance; 
+    }
+
+    public void display() {
+        System.out.println("Account No: " + accountNumber + ", Name: " + name + ", Balance: " + balance);
+    }
+
+}
