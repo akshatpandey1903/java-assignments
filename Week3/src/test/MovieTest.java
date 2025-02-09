04016def3ca351413de13168639c25c9493e7d8b
Akshat Pandey <akshatp1903@gmail.com>
2025-02-09 23:34:19 +0530

    model and test packages and MovieApp code

---
+++ Week3/src/test/MovieTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import model.MovieController;
+
+public class MovieTest {
+    public static void main(String[] args) {
+        MovieController controller = new MovieController();
+        controller.start();
+    }
+}
+
