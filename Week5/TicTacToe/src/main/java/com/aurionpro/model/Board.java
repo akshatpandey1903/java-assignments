108932a9656f240cbfbf38b59523371c098a43b0
Akshat Pandey <akshatp1903@gmail.com>
2025-02-21 01:30:08 +0530

    display updated

--- Week5/TicTacToe/src/main/java/com/aurionpro/model/Board.java
+++ Week5/TicTacToe/src/main/java/com/aurionpro/model/Board.java
@@ -10,7 +10,9 @@ public class Board {
 
     public void boardDisplay(){
         System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
+        System.out.println("----------");
         System.out.println(board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println("----------");
         System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
     }
 
