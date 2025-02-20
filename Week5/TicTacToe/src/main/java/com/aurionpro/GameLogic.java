d92fd49c3f63f0abd2992409091022bb33718bbd
Akshat Pandey <akshatp1903@gmail.com>
2025-02-20 23:51:20 +0530

    Implemented GameLogic and added implementation for GameLogicTest

---
+++ Week5/TicTacToe/src/main/java/com/aurionpro/GameLogic.java
@@ -0,0 +1,32 @@
+package com.aurionpro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameLogic {
+
+    public boolean checkWin(char[] board, char mark){
+        Integer[][] winPatterns = {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                {0, 4, 8}, {2, 4, 6}
+        };
+
+        for(Integer[] pattern : winPatterns){
+            if(board[pattern[0]] == mark && board[pattern[1]] == mark && board[pattern[2]] == mark){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isDraw(char[] board){
+        for(char c : board){
+            if(c != 'X' && c != 'O')
+                return false;
+        }
+        return true;
+    }
+
+}
--- Week5/TicTacToe/src/test/java/com/aurionpro/GameLogicTest.java
+++ Week5/TicTacToe/src/test/java/com/aurionpro/GameLogicTest.java
@@ -1,38 +1,40 @@
 package com.aurionpro;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameLogicTest {
 
-    @Test
-    public void testWinningConditionRow() {
-        fail("Not implemented yet");
+    GameLogic gameLogic;
+
+    @BeforeEach
+    void init(){
+        gameLogic = new GameLogic();
     }
 
     @Test
-    public void testWinningConditionColumn() {
-        fail("Not implemented yet");
-    }
+    public void testWinningCondition() {
+        char[] boardRow = {'X', 'X', 'X', '4', '5', '6', '7', '8', '9'};
+        char[] boardColun = {'X', '2', '3', 'X', '5', '6', 'X', '8', '9'};
+        char[] boardDiagonal = {'X', '2', '3', '4', 'X', '6', '7', '8', 'X'};
 
-    @Test
-    public void testWinningConditionDiagonal() {
-        fail("Not implemented yet");
+        assertTrue(gameLogic.checkWin(boardRow, 'X'));
+        assertTrue(gameLogic.checkWin(boardColun, 'X'));
+        assertTrue(gameLogic.checkWin(boardDiagonal, 'X'));
     }
 
     @Test
     public void testNoWinner() {
-        fail("Not implemented yet");
+        char[] boardDiagonal = {'X', '2', '3', 'X', '5', '6', '7', '8', 'X'};
+        assertFalse(gameLogic.checkWin(boardDiagonal, 'X'));
     }
 
     @Test
     public void testDrawCondition() {
-        fail("Not implemented yet");
-    }
-
-    @Test
-    public void testGameNotDrawWhenEmptyCellsExist() {
-        fail("Not implemented yet");
+        char[] board = {'X', 'O', 'O', 'O', 'O', 'X', 'X', 'X', 'O'};
+        assertFalse(gameLogic.checkWin(board, 'X'));
+        assertTrue(gameLogic.isDraw(board));
     }
 }
 
