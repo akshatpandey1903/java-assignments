1de12a54bea9f20f0dad198186d7cfcf6f3b9c08
Akshat Pandey <akshatp1903@gmail.com>
2025-02-20 23:08:10 +0530

    LogicTest file first implementing Test Driven Development

---
+++ Week5/TicTacToe/src/test/java/com/aurionpro/GameLogicTest.java
@@ -0,0 +1,38 @@
+package com.aurionpro;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GameLogicTest {
+
+    @Test
+    public void testWinningConditionRow() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testWinningConditionColumn() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testWinningConditionDiagonal() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testNoWinner() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testDrawCondition() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testGameNotDrawWhenEmptyCellsExist() {
+        fail("Not implemented yet");
+    }
+}
+
